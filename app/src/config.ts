788330db99ca5a5d3f5166e9797a96f525da1791
rzrsg8 <lukasz.jurkiewicz125@gmail.com>
2024-04-18 19:10:05 +0200

    VPN

--- app/src/config.ts
+++ app/src/config.ts
@@ -15,42 +15,12 @@ export default defineConfig({
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Welcome to Telegram Onboarding Kit',
-          description:
-            "Create stunning onboarding and paywall for your Telegram Bot using the full power of Mini Apps<br><br>It's <b>simple</b>, <b>fast</b>, highly <b>customizable</b> and <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>open-source</a>!",
+          title: 'MyVPN - the best VPN for you',
+          description:"Get access to all the content",
           button: 'Next',
         },
 
-        // image
-        {
-          media: {
-            type: 'image',
-            src: import('./assets/img/durov.webp'),
-          },
-          shape: 'rounded',
-          pagination: 'count',
-          title: 'Onboarding supports many types of content',
-          description:
-            "Here you can see <b>Image</b>. But it's just the beginning...",
-          button: 'Next',
-        },
-
-        // sticker
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_love.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Telegram stickers',
-          description:
-            'Just download any <b>.tgs</b> sticker from Telegram and use it in your onboardings',
-          button: 'Next',
-        },
-
-        // form
+         // form
         {
           extends: 'form', // note, it's important to extend from 'form' here
           media: {
@@ -60,128 +30,44 @@ export default defineConfig({
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Forms',
+          title: 'Why use a VPN?',
           description: 'User fills in the form – the bot receives the data',
           form: [
             {
-              id: 'text_from_form',
-              placeholder: 'Text input',
-              type: 'text',
+              id: 'privacy',
+              placeholder: 'Protect your privacy',
+              type: 'checkbox',
             },
             {
-              id: 'number_from_form',
-              placeholder: 'Number input',
-              type: 'number',
+              id: 'security',
+              placeholder: 'Acess blocked content',
+              type: 'checkbox',
             },
             {
-              id: 'checkbox_from_form',
-              placeholder: 'Checkbox',
+              id: 'other',
+              placeholder: 'Other',
               type: 'checkbox',
             },
           ],
           button: 'Next',
         },
-
-        // video
+        // image
         {
           media: {
-            type: 'video',
-            src: import('./assets/videos/spongebob.mp4'),
-            poster: import('./assets/img/spongebob_poster.webp'),
-            style: 'aspect-ratio: 400/287', // here we manually set video aspect-ratio (default is 16:9)
+            type: 'image',
+            src: import('./assets/img/fenix-games.webp'),
           },
           shape: 'rounded',
           pagination: 'count',
-          title: 'Videos',
-          description:
-            "Typically, video starts <b>automatically</b><br><br>However, on iOS, it will only autoplay upon any prior tap on the page ('Next' button doesn't count). If video doesn't autoplay, user will see preview and pretty animation, inviting them to tap to play the video",
-          button: 'Next',
-        },
-
-        // list
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_juggling.tgs'),
-            size: 150,
+          title: 'People trust us',
+          description: "It's just the beginning",
+          button: {
+            content: "Go to paywall",
+            to: '/paywall',
           },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Lists',
-          description:
-            'Lists can be used to showcase <b>features</b> of your product. Items support customizable icons',
-          list: [
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/guide.svg'),
-                size: 30,
-              },
-              text: 'Some cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/track.svg'),
-                size: 30,
-              },
-              text: 'Some very cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/time.svg'),
-                size: 30,
-              },
-              text: 'Some extremely cool feature',
-            },
-          ],
-          button: 'Next',
         },
 
-        // "everything is customizable" slide
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_xray.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Everything is customizable',
-          description: '',
-          textAlign: 'center',
-          list: [
-            '<b>CSS styles</b>: extend primary colors from Telegram or set yours',
-            'Button text and actions (look down)',
-            'Use our carefully crafted <b>presets</b> or easily create your own',
-          ],
-          button: 'Super-Duper Next',
-        },
-
-        // slide with other features
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_cool.tgs'),
-            size: 150,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Some other features:',
-          description: '',
-          list: [
-            'One-click 0$ <b>deploy</b> on GitHub Pages',
-            'Language and currency localization',
-            'Buttons with <b>haptic</b> feedback',
-            'Content pre-loading for high speed',
-            '<b>Low-code</b> approach to building onboardings',
-            'Many examples/presets',
-            "And many more... (see <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>GitHub</a>)",
-          ],
-          button: 'Next',
-        },
-
+    
         // go to paywall slide
         {
           media: {
@@ -212,33 +98,26 @@ export default defineConfig({
         size: 150,
       },
       shape: 'square',
-      title: 'Your beautiful Paywall',
+      title: 'Unlocka all features',
       list: [
-        'Adjustable product cards',
-        '<b>👛 Wallet Pay</b> and <b>Telegram Payments</b> ready. Add custom methods easily',
-        'Subscriptions or One-time payments',
+        'Unlimited traffic',
+        'No logs',
+        'No ads',
       ],
       products: [
         {
           id: '1_month_subscription',
           title: '1 month subscription',
-          description: '2$/month',
+          description: '1$/month',
           discount: '',
-          price: 2,
+          price: 1,
         },
         {
           id: '1_year_subscription',
           title: '1 year subscription',
-          description: '1$/month',
+          description: '6$/month',
           discount: 'Discount 50%',
-          price: 12,
-        },
-        {
-          id: 'lifetime_access',
-          title: 'Lifetime access',
-          description: '20$ once',
-          discount: 'Best offer',
-          price: 20,
+          price: 6,
         },
       ],
       mainButtonText: 'Buy for {price}',
